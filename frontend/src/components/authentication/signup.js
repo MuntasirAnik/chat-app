600d47104df07d248ee53bd8466a872f62bc3914
MuntasirAnik <anik.muntasir005@gmail.com>
2024-01-23 22:27:40 +0600

    feat: create sign up page

---
+++ frontend/src/components/authentication/signup.js
@@ -0,0 +1,92 @@
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputRightElement,
+  VStack,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+
+const Signup = () => {
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [confirmPassword, setConfirmPassword] = useState();
+  const [pic, setPic] = useState();
+  const [show, setShow] = useState(false);
+
+  const handleClick = () => setShow(!show);
+  const postDetails = (pics) => {};
+  const submitHandler = () => {};
+
+  return (
+    <VStack spacing={"5px"}>
+      <FormControl id="firstName" isRequired>
+        <FormLabel mb={"0px"}>Name</FormLabel>
+        <Input
+          mt={"0px"}
+          placeholder="enter your name"
+          onChange={(e) => setName(e.target.value)}
+        />
+      </FormControl>
+      <FormControl id="email" isRequired>
+        <FormLabel>email</FormLabel>
+        <Input
+          placeholder="enter your email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </FormControl>
+      <FormControl id="password" isRequired>
+        <FormLabel>Password</FormLabel>
+        <InputGroup>
+          <Input
+            type={show ? "text" : "password"}
+            placeholder="enter your password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <InputRightElement width={"4.5rem"}>
+            <Button h={"1.75rem"} size={"sm"} onClick={handleClick}>
+              {show ? "Hide" : "Show"}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </FormControl>
+      <FormControl id="confirmPassword" isRequired>
+        <FormLabel>Confirm Password</FormLabel>
+        <InputGroup>
+          <Input
+            type={show ? "text" : "password"}
+            placeholder="confirm password"
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+          <InputRightElement width={"4.5rem"}>
+            <Button h={"1.75rem"} size={"sm"} onClick={handleClick}>
+              {show ? "Hide" : "Show"}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </FormControl>
+      <FormControl id="Pic" isRequired>
+        <FormLabel>Upload your picture</FormLabel>
+        <Input
+          type="file"
+          p={"1.5"}
+          accept="image/*"
+          onChange={(e) => postDetails(e.target.files[0])}
+        />
+      </FormControl>
+      <Button
+        colorScheme="blue"
+        width={"100%"}
+        style={{ marginTop: 15 }}
+        onClick={submitHandler}
+      >
+        Sign up
+      </Button>
+    </VStack>
+  );
+};
+
+export default Signup;
--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -1,7 +1,63 @@
 import React from "react";
+import backgroundImage from "../../src/images/bg-img.jpeg";
+import {
+  Container,
+  Box,
+  Text,
+  Tabs,
+  Tab,
+  TabPanels,
+  TabPanel,
+  TabList,
+} from "@chakra-ui/react";
+
+import Login from "../components/authentication/login";
+import Signup from "../components/authentication/signup";
 
 const Homepage = () => {
-  return <div className="text-3xl font-bold">Home</div>;
+  return (
+    <div
+      className="text-3xl font-bold h-screen w-screen bg-cover bg-center relative"
+      style={{ backgroundImage: `url(${backgroundImage})` }}
+    >
+      <Container maxW={"xl"} centerContent>
+        <Box
+          display={"flex"}
+          justifyContent={"center"}
+          p={"3"}
+          bg={"white"}
+          w={"100%"}
+          m={"40px 0 15px 0"}
+          borderRadius={"lg"}
+          borderWidth={"1px"}
+        >
+          <Text>Let's Chat</Text>
+        </Box>
+        <Box
+          bg={"white"}
+          w={"100%"}
+          p={"4"}
+          borderRadius={"lg"}
+          borderWidth={"1px"}
+        >
+          <Tabs variant="soft-rounded">
+            <TabList mb={"1em"}>
+              <Tab width={"50%"}>Login</Tab>
+              <Tab width={"50%"}>Sign Up</Tab>
+            </TabList>
+            <TabPanels>
+              <TabPanel>
+                <Login />
+              </TabPanel>
+              <TabPanel>
+                <Signup />
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
+        </Box>
+      </Container>
+    </div>
+  );
 };
 
 export default Homepage;
