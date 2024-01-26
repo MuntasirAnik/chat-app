c7d73bb72257226d27641315d78d0d78d6cd3030
MuntasirAnik <anik.muntasir005@gmail.com>
2024-01-26 22:45:32 +0600

    feat: intall mongodb and connect the backend with the server

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,15 @@
-const express = require('express')
-const dotenv = require('dotenv')
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/dbconfig");
+const colors = require("colors");
 
 const app = express();
 dotenv.config();
+connectDB();
 
-app.get("/" , (req, res)=>{
-    res.send("api is running")
-})
+app.get("/", (req, res) => {
+  res.send("api is running");
+});
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, console.log(`server is running at port ${PORT}`))
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`server is running at port ${PORT}`.yellow.bold));
