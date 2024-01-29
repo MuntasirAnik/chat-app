0e484fce0e7f1c84b88add4976181b347e539351
MuntasirAnik <anik.muntasir005@gmail.com>
2024-01-29 21:10:00 +0600

    feat: authorizatin middileware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const { protect } = require("../middleware/authMiddleware");
 
 const userRouter = express.Router();
 
 const registrationController = require("../controllers/userControllers");
 
 userRouter.get("/get-user", registrationController.getUsers);
+userRouter.get("/all-users", protect, registrationController.allUser);
 userRouter.post("/register", registrationController.registerUser);
 userRouter.post("/login", registrationController.login);
 
