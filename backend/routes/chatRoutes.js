23774e25119188b75dec0562c80377e40b756046
MuntasirAnik <anik.muntasir005@gmail.com>
2024-01-29 22:09:26 +0600

    feat: create one on one chat

---
+++ backend/routes/chatRoutes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const { protect } = require("../middleware/authMiddleware");
+
+const chatRouter = express.Router();
+
+const chatController = require("../controllers/chatController");
+
+chatRouter.post("/create-chat", protect, chatController.createChat);
+// chatRouter.get("/all-users", protect, registrationController.allUser);
+// chatRouter.post("/create-group", protect, createGroupChat);
+// chatRouter.put("/rename-group", protect, renameGroup);
+// chatRouter.put("/remove-group", protect, removefromGroup);
+// chatRouter.put("/add-group", protect, addToGroup);
+
+module.exports = chatRouter;
--- backend/routes/router.js
+++ backend/routes/router.js
@@ -3,8 +3,12 @@ const express = require("express");
 const router = express.Router();
 
 const userRouter = require("./userRoutes");
+const chatRouter = require("./chatRoutes");
 
 //auth
 router.use("/auth", userRouter);
+
+//chats
+router.use("/chats", chatRouter);
 
 module.exports = router;
